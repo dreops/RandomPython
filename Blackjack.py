ba188bd98c94d431fbb14ef817fe67ed2f838f46
dreops <61212907+dreops@users.noreply.github.com>
2020-02-21 10:30:57 +0000

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -3,6 +3,8 @@ import random
 
 deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
 
+# DreOps change (part of training)
+
 def deal(deck): #function for dealing 2 cards to player and dealer at start
     hand = []
     for i in range(2): #2 times loop
